6d5f49658b02b362eaf10daf8819ae48c5f963f8
caohoa <caohoavo22@gmail.com>
2024-10-22 21:18:52 +0800

    Simple Rxjava with CompositeDisposable

--- TestRxjava/app/src/main/java/com/example/testrxjava/MainActivity.java
+++ TestRxjava/app/src/main/java/com/example/testrxjava/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,11 +17,13 @@ import org.w3c.dom.Text;
 import java.util.List;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
     private TextView txtResult, txtUsersResult;
+    private CompositeDisposable compositeDisposablel;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -36,11 +39,13 @@ public class MainActivity extends AppCompatActivity {
 
         txtResult = findViewById(R.id.txtResult);
         txtUsersResult = findViewById(R.id.txtUsersResult);
+        compositeDisposablel = new CompositeDisposable();
         // Using Single
         //fetchMainProductName();
         // Using Observable
-        fetchMainProductName1();
-        fetchUsers();
+        //fetchMainProductName1();
+        //fetchUsers();
+        fetchAllData();
     }
 
     @SuppressLint("CheckResult")
@@ -96,16 +101,67 @@ public class MainActivity extends AppCompatActivity {
                 );
     }
 
+    private void fetchAllData() {
+        compositeDisposablel.add(new ApiService().getMainProductName()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        mainProductName -> {
+                            Log.d(TAG, "main product name = " + mainProductName);
+                        },
+                        throwable -> {
+                            Log.d(TAG, "error = " + throwable.getMessage());
+                        }
+                )
+        );
+        compositeDisposablel.add(new ApiService().getMainProductName1()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        mainProductName -> {
+                            Log.d(TAG, "main product name 1 = " + mainProductName);
+                            updateUi(mainProductName);
+                        },
+                        throwable -> {
+                            Log.d(TAG, "error = " + throwable.getMessage());
+                        },
+                        () -> {
+                            Log.d(TAG, "Task is successful");
+                        }
+                )
+        );
+        compositeDisposablel.add(new ApiService().getUsers()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        users -> {
+                            updateUserUi(users);
+                        },
+                        throwable -> {
+                            Log.d(TAG, throwable.getMessage());
+                        },
+                        () -> {
+                            Log.d(TAG, "Task is completed");
+                        }
+                )
+        );
+    }
+
     public void updateUi(String newText) {
         txtResult.setText(newText);
     }
 
     public void updateUserUi(List<User> users) {
         String msg = "";
-        for (User user: users) {
+        for (User user : users) {
             msg += user.getName() + " - " + user.getEmail() + "\n";
         }
         txtUsersResult.setText(msg);
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        compositeDisposablel.clear();
+    }
 }
