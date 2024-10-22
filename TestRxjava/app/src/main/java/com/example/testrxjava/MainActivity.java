a02d2eebdd52f0a514607d3d8df36120ae67f9f0
caohoa <caohoavo22@gmail.com>
2024-10-22 20:40:40 +0800

    Add updating UI

--- TestRxjava/app/src/main/java/com/example/testrxjava/MainActivity.java
+++ TestRxjava/app/src/main/java/com/example/testrxjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testrxjava;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
+    private TextView txtResult;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,21 +31,26 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        txtResult = findViewById(R.id.txtResult);
         fetchMainProductName();
     }
 
     @SuppressLint("CheckResult")
-    private static void fetchMainProductName() {
+    private void fetchMainProductName() {
         new ApiService().getMainProductName()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         mainProductName -> {
                             Log.d(TAG, "main product name = "+ mainProductName);
+                            updateUi(mainProductName);
                         },
                         throwable -> {
                             Log.d(TAG, "error = "+ throwable.getMessage());
                         }
                 );
+    }
+    public void updateUi(String newText) {
+        txtResult.setText(newText);
     }
 }
