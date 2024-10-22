02efdc454d6881bf6633d580b8d2292f8299eda5
caohoa <caohoavo22@gmail.com>
2024-10-22 21:10:50 +0800

    Simple Rxjava fetch Users

--- TestRxjava/app/src/main/java/com/example/testrxjava/ApiService.java
+++ TestRxjava/app/src/main/java/com/example/testrxjava/ApiService.java
@@ -1,12 +1,15 @@
 package com.example.testrxjava;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Single;
 
 public class ApiService {
     public Single<String> getMainProductName() {
         return Single.create(emitter -> {
-            // Simulate get product names from the internet
+            // Simulate to get product names from the internet
             try {
                 Thread.sleep(2000);
             } catch (Exception e) {
@@ -28,6 +31,26 @@ public class ApiService {
 
             // Emit data
             emitter.onNext("Audi");
+            emitter.onComplete();
+        });
+    }
+    public Observable<List<User>> getUsers() {
+        return Observable.create(emitter -> {
+            // Simulate to get users from the internet
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                emitter.onError(e);
+            }
+
+            // List of users
+            List<User> users = new ArrayList<>();
+            users.add(new User("Alex", "abc@hotmail.com"));
+            users.add(new User("Alice", "def@hotmail.com"));
+            users.add(new User("Bob", "klm@hotmail.com"));
+
+            // Emit data
+            emitter.onNext(users);
             emitter.onComplete();
         });
     }
--- TestRxjava/app/src/main/java/com/example/testrxjava/MainActivity.java
+++ TestRxjava/app/src/main/java/com/example/testrxjava/MainActivity.java
@@ -13,12 +13,15 @@ import androidx.core.view.WindowInsetsCompat;
 
 import org.w3c.dom.Text;
 
+import java.util.List;
+
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
     private final static String TAG = "MainActivity";
-    private TextView txtResult;
+    private TextView txtResult, txtUsersResult;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +35,12 @@ public class MainActivity extends AppCompatActivity {
         });
 
         txtResult = findViewById(R.id.txtResult);
+        txtUsersResult = findViewById(R.id.txtUsersResult);
         // Using Single
         //fetchMainProductName();
         // Using Observable
         fetchMainProductName1();
+        fetchUsers();
     }
 
     @SuppressLint("CheckResult")
@@ -45,14 +50,15 @@ public class MainActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         mainProductName -> {
-                            Log.d(TAG, "main product name = "+ mainProductName);
+                            Log.d(TAG, "main product name = " + mainProductName);
                             updateUi(mainProductName);
                         },
                         throwable -> {
-                            Log.d(TAG, "error = "+ throwable.getMessage());
+                            Log.d(TAG, "error = " + throwable.getMessage());
                         }
                 );
     }
+
     @SuppressLint("CheckResult")
     private void fetchMainProductName1() {
         new ApiService().getMainProductName1()
@@ -60,18 +66,46 @@ public class MainActivity extends AppCompatActivity {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         mainProductName -> {
-                            Log.d(TAG, "main product name 1 = "+ mainProductName);
+                            Log.d(TAG, "main product name 1 = " + mainProductName);
                             updateUi(mainProductName);
                         },
                         throwable -> {
-                            Log.d(TAG, "error = "+ throwable.getMessage());
+                            Log.d(TAG, "error = " + throwable.getMessage());
                         },
                         () -> {
                             Log.d(TAG, "Task is successful");
                         }
                 );
     }
+
+    @SuppressLint("CheckResult")
+    private void fetchUsers() {
+        new ApiService().getUsers()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        users -> {
+                            updateUserUi(users);
+                        },
+                        throwable -> {
+                            Log.d(TAG, throwable.getMessage());
+                        },
+                        () -> {
+                            Log.d(TAG, "Task is completed");
+                        }
+                );
+    }
+
     public void updateUi(String newText) {
         txtResult.setText(newText);
     }
+
+    public void updateUserUi(List<User> users) {
+        String msg = "";
+        for (User user: users) {
+            msg += user.getName() + " - " + user.getEmail() + "\n";
+        }
+        txtUsersResult.setText(msg);
+    }
+
 }
